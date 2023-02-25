db3d346b90dbb359927432dda06bf0aae8e4cdf3
Srija Mukherjee <srijamukherjee@Srijas-MBP-2.modem>
2023-02-26 09:38:08 +1100

    Updates

---
+++ precache-manifest.8a3f0ec96ba610be64dc61e04dd5bba1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2909ff4b4e65421074d8",
+    "url": "/my-website/static/css/main.97d02424.chunk.css"
+  },
+  {
+    "revision": "2909ff4b4e65421074d8",
+    "url": "/my-website/static/js/main.2f95d4a3.chunk.js"
+  },
+  {
+    "revision": "daaceaf34ca0e9640574",
+    "url": "/my-website/static/js/runtime~main.66adf037.js"
+  },
+  {
+    "revision": "c3fb9c30537f8eb715e6",
+    "url": "/my-website/static/css/2.1b703d0d.chunk.css"
+  },
+  {
+    "revision": "c3fb9c30537f8eb715e6",
+    "url": "/my-website/static/js/2.79c4d26f.chunk.js"
+  },
+  {
+    "revision": "a22dead9e37845482c73",
+    "url": "/my-website/static/js/3.d5f08cf4.chunk.js"
+  },
+  {
+    "revision": "66e335e3bb7db219d38997cc66a24bc4",
+    "url": "/my-website/index.html"
+  }
+];
